1d3bd803fa8403514a11058c77e74e315e218217
Ugwu Obed <vjazzyvictor@gmail.com>
2022-10-07 21:37:05 +0100

    search api fix

--- index.php
+++ index.php
@@ -10,10 +10,10 @@ $homeResponse = $client->sendRequest(
     $messageFactory->createRequest('GET', 'http://httplug.io')
 );
 
-var_dump($homeResponse->getStatusCode()); // 200, hopefully
+var_dump($homeResponse->getStatusCode()); 
 
 $missingPageResponse = $client->sendRequest(
     $messageFactory->createRequest('GET', 'http://httplug.io/missingPage')
 );
 
-var_dump($missingPageResponse->getStatusCode()); // 404 ?> 
+var_dump($missingPageResponse->getStatusCode());  ?> 
